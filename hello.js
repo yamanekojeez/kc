a87ef0082ee07fe58eafad96bfe93d8989fae484
EC2 Default User <ec2-user@ip-172-31-40-83.ap-northeast-1.compute.internal>
2019-08-14 11:32:29 +0000

    no comment

--- hello.js
+++ hello.js
@@ -1,5 +1,6 @@
+// Node.jsのサンプル
 console.log('Hello, World!');
 console.log('The sum of 2 and 3 is 5.');
-var sum = parseInt(process.argv[2], 10) + parseInt(process.argv[3], 10);
+let sum = parseInt(process.argv[2], 10) + parseInt(process.argv[3], 10);
 console.log('The sum of ' + process.argv[2] + ' and ' + process.argv[3] + ' is ' + sum + '.');
 
