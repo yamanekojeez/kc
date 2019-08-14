55579011bbfd26d4dde771cf0e5ddb5955a63956
EC2 Default User <ec2-user@ip-172-31-40-83.ap-northeast-1.compute.internal>
2019-08-14 08:26:46 +0000

    getopt関数の使用例

--- getopt.c
+++ getopt.c
@@ -1,47 +1,43 @@
 #include <stdio.h>
 #include <unistd.h>
 
-static void print_arg(int argc, char* argv[]) {
-#if 0
-    int i;
-    for (i=0; i<argc; i++) {
-        printf("argv[%d]=%s ", i, argv[i]);
-    }
-#endif
-}
-
-int main(int argc, char* argv[]) {
-    int c;
-    const char* optstring = "ab:" ; // optstringを定義します
-    opterr = 0;                     // disable error log
-
-    /* non-option or end of argument list or error('?')までloop */
-    while((c=getopt(argc, argv, optstring)) != -1) {
-        print_arg(argc, argv);
-        printf("opt=%c ", c);
-        if(c == 'a') {
-            /* without option argument */
-        } else if(c == 'b') {
-            /* with option argument */
-            printf("optarg=%s ", optarg);
-        } else {
-            printf("optopt=%c ", optopt);
-            if(optopt == 'b') {
-                /* no option argument */
-            } else {
-                /* unknown option character */
-            }
-            printf("\n");
-            return -1; // error
-        }
-        printf("\n");
-    }
-
-    /* non-option or end of argument list */
-    while(optind < argc) {
-        print_arg(argc, argv);
-        printf("non-opt=%s \n", argv[optind]);
-        optind++;
-    }
-    return 0;
+/* getopt関数の使用例 */
+int main(int argc, char*argv[]) {
+	int i;
+	int aopt = 0;
+	int bopt = 0;
+	int copt = 0;
+	int dopt = 0;
+	char *cparam = NULL;
+	char *dparam = NULL;
+	int opt;
+	while ((opt = getopt(argc, argv, "abc:d::")) != -1) {
+		switch (opt) {
+			case 'a':
+				aopt = 1;
+				break;
+			case 'b':
+				bopt = 1;
+				break;
+			case 'c':
+				copt = 1;
+				cparam = optarg;
+				break;
+			case 'd':
+				dopt = 1;
+				dparam = optarg;
+				break;
+			default:
+				printf("Error! \'%c\' \'%c\'\n", opt, optopt);
+				return 1;
+		}
+	}
+	printf("a = %d\n", aopt);
+	printf("b = %d\n", bopt);
+	printf("c = %d, %s\n", copt, cparam);
+	printf("d = %d, %s\n", dopt, dparam);
+	for (i = optind; i < argc; i++) {
+		printf("arg = %s\n", argv[i]);
+	}
+	return 0;
 }
