e3afbe4d3a6e8b893e1c56f1c89b4349b4b8bd7a
EC2 Default User <ec2-user@ip-172-31-40-83.ap-northeast-1.compute.internal>
2019-08-17 07:30:06 +0000

    入力された文字を標準出力する処理に変更

--- hello.js
+++ hello.js
@@ -1,5 +1,35 @@
-// Node.jsのサンプル
-console.log('Hello, World!');
-console.log('The sum of 2 and 3 is 5.');
-let sum = parseInt(process.argv[2], 10) + parseInt(process.argv[3], 10);
-console.log('The sum of ' + process.argv[2] + ' and ' + process.argv[3] + ' is ' + sum + '.');
+/* Node.jsのサンプル */
+console.log('何か入力してください!(endは終了)');
+
+/* 入力待ち状態にする*/
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+
+/* 入力終了処理 */
+process.stdin.on('end', function() {
+	console.log('入力終了');
+});
+
+/* 入力された1行を読み込んだ時のイベント処理 */
+process.stdin.on('data', function(inputData) {
+	/* 末尾の改行除去 */
+	let input = inputData.slice(0, -1);
+		if (input == 'end') {
+			process.exit(0);
+		} else {
+			console.log('入力文字: ' + input);
+			process.exit(0);
+		}
+});
+
+/* Ctrl+Cが入力された場合のイベント処理 */
+process.on('SIGINT', function() {
+	/* プロセス終了 */
+	console.log('');
+	process.exit(0);
+});
+
+/* プロセス終了時のイベント処理 */
+process.on('exit', function() {
+	console.log('終了!!');
+});
