e351979c985c5182e5e2d5188163aae84053f8b3
EC2 Default User <ec2-user@ip-172-31-40-83.ap-northeast-1.compute.internal>
2020-07-14 06:47:18 +0000

    Changes to be committed: 	new file:   crlfcheck.sh 	new file:   log/crlfcheck.sh.log 	new file:   log/crlfcheck.sh_real.log

---
+++ crlfcheck.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+if [ $# != 1 ]; then
+  echo "ERROR 引数に誤りがあります、処理を中断します"
+  echo "Usage：crlfcheck.sh [ チェック対象のディレクトリ名 ]"
+  exit
+fi
+
+SHNM=$(basename $0)
+
+rm -f ./log/${SHNM}.log > /dev/null 2>&1
+rm -f ./log/${SHNM}_real.log > /dev/null 2>&1
+
+mkdir ./log > /dev/null 2>&1
+
+touch ./log/${SHNM}.log > /dev/null 2>&1
+touch ./log/${SHNM}_real.log > /dev/null 2>&1
+
+echo "START $(date)"
+
+for file in `\find $1 -type f`; do
+
+  file ${file} >> ./log/${SHNM}_real.log 2>&1
+  CRLFCOUNT=`file ${file} | grep -c "CRLF"`
+
+  if [ ${CRLFCOUNT} -eq "0" ];then
+    echo "LF,${file}" >> ./log/${SHNM}.log 2>&1
+  else
+    echo "CRLF,${file}" >> ./log/${SHNM}.log 2>&1
+  fi
+
+done
+
+echo "結果：CRLFのファイルは$(cat ./log/${SHNM}.log | grep -c "CRLF")件でした"
+echo "END $(date)"
